7be09becacb279e88b1f17a240ce47672add6195
jesufemi-o <ogunwedeemmanuel@gmail.com>
2021-12-28 12:13:08 +0100

    fixed cors and setup autodeployment

--- server/custom_server/main.py
+++ server/custom_server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from custom_server.api.router import api_router
 from custom_server.core.settings import (API_PREFIX, APP_NAME, APP_VERSION,
@@ -7,8 +8,18 @@ from custom_server.core.events import (start_app_handler,
                                                   stop_app_handler)
 
 
+CORS_ORIGINS = ["*"]
+
+
 def get_app() -> FastAPI:
     fast_app = FastAPI(title=APP_NAME, version=APP_VERSION, debug=IS_DEBUG)
+    fast_app.add_middleware(
+        CORSMiddleware,
+        allow_origins=CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     fast_app.include_router(api_router, prefix=API_PREFIX)
 
     fast_app.add_event_handler("startup", start_app_handler(fast_app))
--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,4 @@
 gcloud config set run/platform managed
 gcloud config set run/region us-central1
-gcloud builds submit --tag gcr.io/<PROJECT_ID>/<SERVICE_ID>
-gcloud run deploy --image gcr.io/<PROJECT_ID>/<SERVICE_ID>
+gcloud builds submit --tag gcr.io/fireloans-bff91/fireloans
+gcloud run deploy --image gcr.io/fireloans-bff91/fireloans
